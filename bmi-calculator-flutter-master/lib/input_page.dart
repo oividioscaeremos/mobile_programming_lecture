7f96736c2f81becfb61f21b091d9a4b19d2f10d5
İlker Atabay <ilkeratabay@yahoo.com>
2019-11-25 23:50:18 +0300

    25 Kasım Calculator Başlangıç

---
+++ bmi-calculator-flutter-master/lib/input_page.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'BMI CALCULATOR',
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(color: Color(0xff090f32)),
+        child: Center(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Expanded(
+                      child: Container(
+                        child: Text("Merhaba"),
+                        margin: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Color(0xff141a3c),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: Text("Merhaba"),
+                        margin: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Color(0xff141a3c),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  child: Text("Merhaba"),
+                  margin: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color(0xff141a3c),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: Theme(
+        data: ThemeData.light(),
+        child: FloatingActionButton(
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
