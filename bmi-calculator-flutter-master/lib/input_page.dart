052803ffe4fd84f026a6676f8b936de7065748e1
İlker Atabay <ilkeratabay@yahoo.com>
2019-12-02 13:50:09 +0300

    2 aralık ders başlamadan önce.

--- bmi-calculator-flutter-master/lib/input_page.dart
+++ bmi-calculator-flutter-master/lib/input_page.dart
@@ -20,7 +20,7 @@ class _InputPageState extends State<InputPage> {
       body: Container(
         decoration: BoxDecoration(color: Color(0xff090f32)),
         child: Center(
-          child: Row(
+          child: Column(
             children: <Widget>[
               Expanded(
                 child: Row(
@@ -45,18 +45,52 @@ class _InputPageState extends State<InputPage> {
                           color: Color(0xff141a3c),
                         ),
                       ),
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Expanded(
+                      child: Container(
+                        child: Text("Merhaba"),
+                        margin: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Color(0xff141a3c),
+                        ),
+                      ),
                     ),
                   ],
                 ),
               ),
               Expanded(
-                child: Container(
-                  child: Text("Merhaba"),
-                  margin: EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Color(0xff141a3c),
-                  ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Expanded(
+                      child: Container(
+                        child: Text("Merhaba"),
+                        margin: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Color(0xff141a3c),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: Text("Merhaba"),
+                        margin: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Color(0xff141a3c),
+                        ),
+                      ),
+                    )
+                  ],
                 ),
               ),
             ],
